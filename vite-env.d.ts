ef62cedaf34a41b0b3e0e93e16e6f9189e1464be
lanryweezy <lanryweezy@gmail.com>
2026-06-19 00:40:05 +0100

    Remove mathjs type declaration from vite-env.d.ts (may block Vite build)

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -1,9 +1,5 @@
 /// <reference types="vite/client" />
 
-declare module 'mathjs' {
-  export function evaluate(expr: string): any;
-}
-
 interface ImportMetaEnv {
     readonly VITE_UNSPLASH_ACCESS_KEY: string;
     readonly VITE_UNSPLASH_SECRET_KEY: string;
