369cb323be004a17bddf577b8086f5955c04d5c7
lanryweezy <lanryweezy@gmail.com>
2026-06-18 23:44:29 +0100

    Remove debug artifacts, stale type error snapshots, and debug scripts

--- fix_eslint.sh
+++
@@ -1,5 +0,0 @@
-echo -e "\nPathEditorOverlay.displayName = 'PathEditorOverlay';" >> components/VectorEditor/PathEditorOverlay.tsx
-echo -e "\nCanvasControls.displayName = 'CanvasControls';" >> components/canvas/CanvasControls.tsx
-echo -e "\nCanvasGuides.displayName = 'CanvasGuides';" >> components/canvas/CanvasGuides.tsx
-echo -e "\nSelectionMarquee.displayName = 'SelectionMarquee';" >> components/canvas/SelectionMarquee.tsx
-echo -e "\nBrushFilters.displayName = 'BrushFilters';" >> services/brushEngine.tsx
