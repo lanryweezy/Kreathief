90f3daf30d926e473e1dffeec1d3644150c2984f
lanryweezy <lanryweezy@gmail.com>
2026-06-23 12:53:33 +0100

    🎨 Fix Slider component JSX structure

--- components/Slider.tsx
+++ components/Slider.tsx
@@ -46,27 +46,7 @@ export const Slider: React.FC<SliderProps> = ({
           value={value}
           onChange={(e) => onChange(Number(e.target.value))}
           disabled={disabled}
-          className="w-full h-1.5 rounded-full appearance-none cursor-pointer
-            bg-surface-dark-4
-            [&::-webkit-slider-thumb]:appearance-none
-            [&::-webkit-slider-thumb]:w-3.5 [&::-webkit-slider-thumb]:h-3.5
-            [&::-webkit-slider-thumb]:rounded-full
-            [&::-webkit-slider-thumb]:bg-brand-600
-            [&::-webkit-slider-thumb]:shadow-lg
-            [&::-webkit-slider-thumb]:shadow-brand-600/30
-            [&::-webkit-slider-thumb]:border-2
-            [&::-webkit-slider-thumb]:border-white/20
-            [&::-webkit-slider-thumb]:transition-all
-            [&::-webkit-slider-thumb]:duration-150
-            [&::-webkit-slider-thumb]:hover:scale-110
-            [&::-webkit-slider-thumb]:hover:bg-brand-500
-            [&::-webkit-slider-thumb]:focus-visible:outline-none
-            [&::-webkit-slider-thumb]:focus-visible:ring-2
-            [&::-webkit-slider-thumb]:focus-visible:ring-brand-400
-            [&::-webkit-slider-thumb]:active:scale-95
-            focus-visible:outline-none
-            focus-visible:ring-2 focus-visible:ring-brand-400/50
-            focus-visible:rounded-full
+          className="w-full h-1.5 rounded-full appearance-none cursor-pointer bg-surface-dark-4 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-brand-400/50"
           style={{
             background: `linear-gradient(to right, #7d2ae8 0%, #7d2ae8 ${percentage}%, #252627 ${percentage}%, #252627 100%)`,
           }}
